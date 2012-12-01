f4ba3cde1c4bbb6cc81cebafc399baa1b77c1699
Bryan Paluch <bryanpaluch@gmail.com>
2012-12-01 16:28:56 -0500

    admin site

--- xbee-cordinator/server.js
+++ xbee-cordinator/server.js
@@ -1,15 +1,29 @@
-var util = require('util');
 var XBee = require('svd-xbee').XBee;
-var request = require('request');
 var xbee = new XBee({port: '/dev/ttyUSB0', baudrate:9600});
 var EventEmitter = require('events').EventEmitter;
 var handler = new EventEmitter();
+var app = require('express')()
+var server = require('http').createServer(app)
+var passport = require('passport')
+require('express-namespace')
+
+
+var env = process.env.NODE_ENV || 'development'
+  , config = require('./config/config')[env]
+
+
+
 //Start the xbee radio
 require('./lib/cordinator.js').boot(handler);
 
 //Connect to cloud service and listen to events
-require('./lib/socket.js').boot(handler);
+require('./lib/cloudconnect.js').boot(handler);
 
 //Manages events
 require('./lib/eventHandler.js').boot(handler);
 
+//Start the admin interface
+require('./config/passport').boot(passport, config)
+require('./config/settings').boot(app, config, passport)
+require('./interfaces/routes')(app, passport);
+server.listen(3000);
