66580659f0ce0994f934dc1a5edc61d39040ff5a
Bryan Paluch <bryanpaluch@gmail.com>
2012-12-06 18:52:11 -0500

    posting to cloud

--- xbee-cordinator/lib/cloudconnect.js
+++ xbee-cordinator/lib/cloudconnect.js
@@ -1,21 +1,39 @@
 ioclient = require('socket.io-client');
 memoryDb = require('./memoryDb.js');
+request = require('request');
 
 exports.boot = function (handler){
-  var cloudinitiated = memoryDb.statusDb.get('cloudinitiated');
+  var cloudinitiated = memoryDb.checkCloudInit('cloudinitiated');
 
 
   handler.on('cloud_save', function(data){
     if(cloudinitiated){
       console.log('saving to cloud');
-      //Post to cloud
+      saveToCloud(data); 
     }else{
-      console.log('cloud not initiated not saving to cloud');
+      cloudinitiated = memoryDb.checkCloudInit('cloudinitiated');
+      if(cloudinitiated){
+        console.log('saving to cloud');
+        saveToCloud(data); 
+      }else{
+        console.log('cloud not initiated not saving to cloud');
+      }
     }
 
 
   });
+  
+  function saveToCloud(data){
+    console.log(data);
+    request('http://home-monitor.bryanpaluch.com/api/sensor/' + data.sensorId, function(error, response, body ){
+      if(!error && response.status == 200){
+        console.log(body);
+      }else{
+        console.log('error', error);
+      }
+    });
 
+  }
 
 
 
